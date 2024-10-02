74d0741f5d1824ff6024a308db1c36f98e5c7b60
JOHAN <johamax@gmail.com>
2024-10-02 12:57:04 -0500

    v1.5 - Implementación de SecondActvity.kt

--- app/src/main/java/com/example/proyecto41/SecondActvity.kt
+++ app/src/main/java/com/example/proyecto41/SecondActvity.kt
@@ -5,10 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyecto41.databinding.ActivitySecondActvityBinding
+import android.content.Intent
+
 
 class SecondActvity : AppCompatActivity() {
+
+    //1. Declaramos la variable binding
+    private lateinit var binding: ActivitySecondActvityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //2. Inicializamos el binding
+        binding = ActivitySecondActvityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //3. Obtenemos el valor del username
+        val username = intent.getStringExtra("username")
+
+        //Mostramos el mensaje de bienvenida
+        binding.tvWelcome.text = "Bienvenido $username!!!"
+
+        //Configuramos el botón para cerrar sesión
+        binding.btnLogout.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_second_actvity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
